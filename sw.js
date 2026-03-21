67b502ee0024f71fe5515a5ea79b6a832ae6acca
C2O2Nut <99393012+C2O2Nut@users.noreply.github.com>
2026-03-21 20:18:54 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,18 @@
-var CACHE = 'sakuhin-log-v1';
-var FILES = ['/', '/index.html'];
+var CACHE = 'sakuhin-log-v3';
+var FILES = ['/', '/sakuhin-log/index.html'];
 
 self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
+  e.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', function(e) {
-  e.waitUntil(caches.keys().then(function(keys) {
-    return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
-  }));
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+    }).then(function() { return self.clients.claim(); })
+  );
 });
 
 self.addEventListener('fetch', function(e) {
-  e.respondWith(caches.match(e.request).then(function(r) {
-    return r || fetch(e.request).then(function(resp) {
-      var clone = resp.clone();
-      caches.open(CACHE).then(function(c) { c.put(e.request, clone); });
-      return resp;
-    }).catch(function() { return caches.match('/index.html'); });
-  }));
+  e.respondWith(fetch(e.request).catch(function() { return caches.match(e.request); }));
 });
